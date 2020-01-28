a955cb8ee7893ae24a0c7855650d26c5703d2dbc
trs1323 <45774674+trs1323@users.noreply.github.com>
2020-01-28 10:58:55 -0700

    fixed links

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ export default class Header extends Component {
     render() {
         return (
             <div className="header">
-                <div><Link to="/home"><p>Home</p></Link></div>
+                <div><Link to="/merch-site/"><p>Home</p></Link></div>
                 <div><Link to="/shop"><p>Shop</p></Link></div>
                 <div><h1>Merch Name</h1></div>
                 <div><Link to="/faq"><p>FAQ</p></Link></div>
